f88e25d6f2b05b4793a0b375310bd4721044b936
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-08-01 01:26:41 -0600

    74: Crear instancia de axios

--- models/busquedas.js
+++ models/busquedas.js
@@ -3,15 +3,28 @@ const axios = require('axios');
 class Busquedas {
 
     historial = ['Cosamaloapan', 'Madrid', 'San José'];
-    
+
     constructor() {
         // TODO: leer DB si existe
     }
 
+    get paramsMapbox() {
+        return {
+            'access_token': 'pk.eyJ1Ijoic2llcnJhMDM0IiwiYSI6ImNsa3J3aW1yejA4c2szcG8wemtlMGNmeWIifQ.lItVw2YKQ3Rfl8CfYbn0Cw',
+            'limit': 5,
+            'language': 'es',
+        }
+    }
+    
     async ciudad(lugar = '') {
         try {
             // TODO: petición http
-            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Xalapa.json?limit=5&language=es&access_token=pk.eyJ1Ijoic2llcnJhMDM0IiwiYSI6ImNsa3J3aW1yejA4c2szcG8wemtlMGNmeWIifQ.lItVw2YKQ3Rfl8CfYbn0Cw');
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox,
+            });
+
+            const resp = await instance.get();
             console.log(resp.data);
             return [];
         } catch (error) {
@@ -21,4 +34,4 @@ class Busquedas {
     }
 }
 
-module.exports= Busquedas;
+module.exports = Busquedas;
